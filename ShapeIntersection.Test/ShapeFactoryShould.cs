f302c36a8566c0e69d23d486765967c8cccef74b
The Viking Software Crafter <sergi@thevikingsoftwarecrafter.com>
2020-03-15 11:00:47 +0100

    recaft: add Autofixture to abstract test from concrete values

--- ShapeIntersection.Test/ShapeFactoryShould.cs
+++ ShapeIntersection.Test/ShapeFactoryShould.cs
@@ -1,3 +1,4 @@
+using AutoFixture.Xunit2;
 using FluentAssertions;
 using ShapeIntersection.Domain.Dimensions;
 using ShapeIntersection.Domain.Factories;
@@ -8,23 +9,20 @@ namespace ShapeIntersection.Test
 {
     public class ShapeFactoryShould
     {
-        [Fact]
-        public void Create_A_New_Cube_When_Parameters_Are_Cube_Dimensions()
+        [Theory, AutoData]
+        public void Create_A_New_Cube_When_Parameters_Are_Cube_Dimensions(float x, float y, float side)
         {
             //Assert
-            var x = new X(0);
-            var y = new Y(0);
-            var side = new Side(1);
-            var cubeDimensions = new CubeDimensions(x, y, side);
+            var cubeDimensions = new CubeDimensions(new X(x), new Y(y), new Side(side));
 
             //Act
             var cube = ShapeFactory.Create(cubeDimensions);
 
             //Arrange
             cube.Should().BeOfType(typeof(Cube));
-            ((Cube) cube).X.Should().Be(x);
-            ((Cube) cube).Y.Should().Be(y);
-            ((Cube) cube).Side.Should().Be(side);
+            ((Cube) cube).X.Value.Should().Be(x);
+            ((Cube) cube).Y.Value.Should().Be(y);
+            ((Cube) cube).Side.Value.Should().Be(side);
         }
     }
 }
