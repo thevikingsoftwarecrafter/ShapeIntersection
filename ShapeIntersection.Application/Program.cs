5335b2d6322f4994cebe864c639135d2e6381d47
The Viking Software Crafter <sergi@thevikingsoftwarecrafter.com>
2020-03-15 12:17:39 +0100

    feat: add console app program to see concrete results of calls and stuff

---
+++ ShapeIntersection.Application/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ShapeIntersection.Domain.Dimensions;
+using ShapeIntersection.Domain.Factories;
+using ShapeIntersection.Domain.Services;
+using ShapeIntersection.Domain.Shapes.Primitives;
+
+namespace ShapeIntersection.Application
+{
+    class Program
+    {
+        private static IIntersectionService _intersectionService;
+
+        private static void Main(string[] args)
+        {
+            DependencyInyection();
+            var cube1 = ShapeFactory.Create(_intersectionService, new CubeDimensions(new X(0), new Y(0), new Side(10)));
+            var cube2 = ShapeFactory.Create(_intersectionService, new CubeDimensions(new X(5), new Y(5), new Side(7)));
+
+            Console.WriteLine("Shape Intersection");
+            Console.WriteLine($"-----------------");
+            Console.WriteLine($"Shape 1: {cube1}");
+            Console.WriteLine($"Shape 2: {cube2}");
+            Console.WriteLine();
+            Console.WriteLine($"Intersection result: \n");
+            Console.WriteLine($"\t{cube1.IntersectWith(cube2)}");
+            Console.ReadKey();
+        }
+
+        private static void DependencyInyection()
+        {
+            _intersectionService = new IntersectionService();
+        }
+    }
+}
